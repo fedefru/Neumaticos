a70aa5ad27fc1604cfb05a788ca64ae93fc3fa81
fru.federico@gmail.com <39113098federico>
2019-12-30 19:15:08 -0300

    agregando productos dinamicos

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, NavigationEnd, NavigationError } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
+
+import { IProduct } from 'app/shared/model/product.model';
+import { ProductService } from '../../entities/product/product.service';
 
 import { JhiLanguageHelper } from 'app/core/language/language.helper';
 
@@ -8,10 +12,15 @@ import { JhiLanguageHelper } from 'app/core/language/language.helper';
   templateUrl: './main.component.html'
 })
 export class JhiMainComponent implements OnInit {
-  items: Array<string>;
+  items: IProduct[];
 
+  constructor(protected productService: ProductService, private jhiLanguageHelper: JhiLanguageHelper, private router: Router) {}
 
-  constructor(private jhiLanguageHelper: JhiLanguageHelper, private router: Router) {}
+  loadAll() {
+    this.productService.query().subscribe((res: HttpResponse<IProduct[]>) => {
+      this.items = res.body;
+    });
+  }
 
   private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
     let title: string = routeSnapshot.data && routeSnapshot.data['pageTitle'] ? routeSnapshot.data['pageTitle'] : 'neumaticosApp';
@@ -22,8 +31,8 @@ export class JhiMainComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.items = ['item1', 'item2', 'item3' ,'item4'];
-
+    //this.items = ['item1', 'item2', 'item3' ,'item4'];
+    this.loadAll();
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
